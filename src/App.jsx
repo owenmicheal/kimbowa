489d7d460d822d97b1bae1e8b52cc19a7211ea3f
Kimbowa Owen <owenmicheal3rd@gmail.com>
2025-04-15 13:52:57 +0300

    implemented the nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Navbar from "./components/Navbar";
 import Hero from "./sections/Hero";
 import ShowCase from "./sections/ShowCase";
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Hero />
       <ShowCase />
     </>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import { navLinks } from "../constants";
+
+const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 10;
+      setScrolled(isScrolled);
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <header className={`navbar ${scrolled ? "scrolled" : "not-scrolled"}`}>
+      <div className="inner">
+        <a href="#hero" className="logo">
+          Kimbowa
+        </a>
+
+        <nav className="desktop">
+          <ul>
+            {navLinks.map(({ name, link }) => (
+              <li key={name} className="group">
+                <a href={link}>
+                  <span>{name}</span>
+
+                  <span className="underline"></span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <a href="#contact" className="contact-btn group">
+          <div className="inner">
+            <span>Contact Me</span>
+          </div>
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
